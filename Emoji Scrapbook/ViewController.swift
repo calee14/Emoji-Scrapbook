5db678cf12d337331adde91445cd83be2cea276e
Cap <caplee1000@gmail.com>
2016-12-06 18:14:41 -0800

    Adding TableView

--- Emoji Scrapbook/ViewController.swift
+++ Emoji Scrapbook/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var Tablets: UITableView!
+    var emoji = ["🖕🏻","😂","😡"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Tablets.dataSource = self
+        Tablets.delegate = self
+        
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "🤑"
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
