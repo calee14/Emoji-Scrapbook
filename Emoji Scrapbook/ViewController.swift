068385642193d014c2d24f67401099ca111c1e64
Cap <caplee1000@gmail.com>
2016-12-06 20:11:05 -0800

    Added Table View and Emojis

--- Emoji Scrapbook/ViewController.swift
+++ Emoji Scrapbook/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var Tablets: UITableView!
-    var emoji = ["🖕🏻","😂","😡"]
+    var emojis = ["🖕🏻","😂","😡","😜","😚","😏","👨‍❤️‍💋‍👨","🐹","👚","👑","🐋","🤑"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,12 +20,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
         let cell = UITableViewCell()
-        cell.textLabel?.text = "🤑"
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
     override func didReceiveMemoryWarning() {
